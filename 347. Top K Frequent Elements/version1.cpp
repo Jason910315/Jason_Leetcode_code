8b3720d128c7296003190416f59e0ab275fcee2d
Jason910315 <a0938692163@gmail.com>
2024-11-25 00:09:53 +0800

    2024/11/24

--- 347. Top K Frequent Elements/version1.cpp
+++ 347. Top K Frequent Elements/version1.cpp
@@ -1,4 +1,4 @@
-// Time:O(n)
+// Time:O(n)，n 為 nums 長度
 // Space:O(n)
 
 #include <iostream>
@@ -11,25 +11,26 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> count;
-        vector<vector<int>> freq;
-        vector<int> ans;
-        for(auto n : nums){
-            count[n]++;
+        vector<vector<int>> freq(nums.size() + 1);  // 要初始化 freq 長度，以免取值 out of bounds
+        for(auto s : nums){
+            count[s]++;  // 紀錄每個數字出現次數
         }
+        // 用 bucket sort，將每個數字填入其出現次數的籃子裡
         for(auto &pair : count){
             freq[pair.second].push_back(pair.first);
         }
 
-        for(int i = freq.size()-1;i >= 0 ;i--){
-            for(int num : freq[i]){
+        vector<int> ans;
+        // 由大到小取回來
+        for(int i = freq.size() - 1;i > 0 ;i--){
+            for(auto num : freq[i]){
                 ans.push_back(num);
                 if(ans.size() == k){
-                    break;
+                    return ans;
                 }
             }
         }
-        return ans;
-        
+        return ans;  
     }
 };
 
